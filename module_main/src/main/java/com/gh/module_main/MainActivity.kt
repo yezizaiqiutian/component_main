49d8096ee91b3703732801dc9415a166b6c73f8a
gaohui01 <gaohui01@5i5j.com>
2022-04-18 17:22:30 +0800

    组件化雏形,修改配置文件

--- module_main/src/main/java/com/gh/module_main/MainActivity.kt
+++ module_main/src/main/java/com/gh/module_main/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.gh.module_main
 
 import android.widget.Toast
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.launcher.ARouter
 import com.gh.lib_core.baseui.BaseBindingActivity
 import com.gh.lib_core.baseui.BaseViewModel
+import com.gh.lib_router.login.LoginRoute
+import com.gh.lib_router.login.LoginService
+import com.gh.lib_router.main.MainRoute
 import com.gh.module_main.databinding.MainActivityMainBinding
 
+@Route(path = MainRoute.ACTIVITY_MAIN)
 class MainActivity : BaseBindingActivity<MainActivityMainBinding, BaseViewModel>() {
 
     override fun getLayoutId() = R.layout.main_activity_main
 
     override fun onInit() {
         binding.btnInterceptor.setOnClickListener {
-            Toast.makeText(this,"aaaa",Toast.LENGTH_SHORT).show()
+
+            ARouter.getInstance().build(LoginRoute.ACTIVITY_LOGIN).navigation()
+
+        }
+        binding.btnIslogin.setOnClickListener {
+
+            val navigation = ARouter.getInstance().navigation(LoginService::class.java)
+            if (navigation != null) {
+                val login = navigation.isLogin()
+                Toast.makeText(this@MainActivity,"是否登录:$login",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
